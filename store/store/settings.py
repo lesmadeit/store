cc0277d1c5a0b301ae98db9f5c97adfee4093c56
lesmadeit <lesmwendwa@gmail.com>
2025-09-05 21:08:01 +0300

    changes

--- store/store/settings.py
+++ store/store/settings.py
@@ -186,7 +186,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-#STATICFILES_DIRS = [BASE_DIR / 'store/static']
+STATICFILES_DIRS = [BASE_DIR / 'store/static']
 
 
 
